8893ee3f8959cff3cbd4c603248857d500a001dc
Manoj Pawar SJ <manojpawarsj@pop-os.localdomain>
2020-02-17 16:44:26 +0530

    clean

--- livewebcam.py
+++ livewebcam.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+import cv2
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -15,36 +16,22 @@ from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets, models, transforms
 
-import cv2
-
 train_on_gpu = torch.cuda.is_available()
 if not train_on_gpu:
     print('CUDA is not available.  Training on CPU ...')
-    
+
 else:
     print('CUDA is available!  Training on GPU ...')
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-#!pip install --upgrade wandb
-
-
-
 
 
 
 test_transforms = transforms.Compose([transforms.Resize(255),
-                                    #  transforms.CenterCrop(224),
-                                       transforms.ToTensor(),
-                                       ])
+                                      #  transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      ])
 
 
-
-
-
-# define samplers for obtaining training and validation batches
-
-
-
-# Pass transforms in here, then run the next cell to see how the transforms look
 
 model = models.densenet161()
 
@@ -63,14 +50,13 @@ scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 model = model.cuda()
 
 model.load_state_dict(torch.load('tensorboardexp.pt'))
-classes=["accident","noaccident"]
-#model.load_state_dict(torch.load('tensorboardexp.pt'))
+classes = ["accident", "noaccident"]
 count = 0
 counts = 1
 vid = cv2.VideoCapture(0)
 ret = True
 while ret:
-    if ret==True:
+    if ret == True:
         ret, frame = vid.read()
 
         try:
@@ -87,25 +73,21 @@ while ret:
             output = model(img)
             _, predicted = torch.max(output, 1)
 
-
-
             index = int(predicted.item())
-            if index==0:
+            if index == 0:
                 cv2.imwrite(r"D:\xamp\htdocs\img\frame%d.png" % count, frame)
-                count+=1
-                if counts==1:
+                count += 1
+                if counts == 1:
                     webbrowser.open('127.0.0.1', new=2)
-                    counts+=1
-                    
+                    counts += 1
 
+            labels = 'status: ' + classes[index]
 
-            labels = 'status: ' +classes[index]
-            
-
-        cv2.putText(frame,labels,(10,100), cv2.FONT_HERSHEY_DUPLEX, 2, (0,0,255), 5, cv2.LINE_AA)
-        cv2.imshow('Frame',frame)
+        cv2.putText(frame, labels, (10, 100),
+                    cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0, 255), 5, cv2.LINE_AA)
+        cv2.imshow('Frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-   
+
 vid.release()
-cv2.destroyAllWindows()    
+cv2.destroyAllWindows()
